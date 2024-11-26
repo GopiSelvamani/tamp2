a3214f0428fbeaa3c36d4e82422ec962ee1fc5c0
GopiSelvamani <gopiselvam1995@gmail.com>
2024-11-26 16:00:22 +0400

    task2

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'room': ["Righteous", 'sans-serif'],
+        'saira': ["Saira", 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
